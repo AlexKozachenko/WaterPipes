450ff2c90c64b62421f61ff136b5830189c7cc19
AlexKozachenko <kozachenko@gmail.com>
2018-05-06 19:16:27 +0300

    Добавьте файлы проекта.

---
+++ WaterPipes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WaterPipes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
